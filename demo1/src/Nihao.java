0b463c27ef7ff6f66a6b42be510055206cef5b5e
ccl <1282750538@qq.com>
2021-09-05 19:44:21 +0800

    f分支创建

--- demo1/src/Nihao.java
+++ demo1/src/Nihao.java
@@ -7,5 +7,10 @@ public class Nihao {
     public static void main(String[] args) {
 
         System.out.println("你好！！！！");
+
+        int res = 0;
+        for (int i = 0; i < 100; i++) {
+            res+=i;
+        }
     }
 }
