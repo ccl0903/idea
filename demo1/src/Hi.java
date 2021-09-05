f6c1d4ff5dacbc55b033d12611de0f44f5cc0385
ccl <1282750538@qq.com>
2021-09-05 19:40:25 +0800

    tianjia

---
+++ demo1/src/Hi.java
@@ -0,0 +1,12 @@
+import sun.tools.jar.Main;
+
+/**
+ * @author chen
+ * @version 1.0
+ * @description
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hi~~");
+    }
+}
---
+++ demo1/src/Nihao.java
@@ -0,0 +1,11 @@
+/**
+ * @author chen
+ * @version 1.0
+ * @description
+ */
+public class Nihao {
+    public static void main(String[] args) {
+
+        System.out.println("你好！！！！");
+    }
+}
