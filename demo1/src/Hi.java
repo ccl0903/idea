2dc253e519eac7156cc7ddf4e34a83d576cdda32
ccl0903 <88029007+ccl0903@users.noreply.github.com>
2021-09-05 20:26:24 +0800

    Update Hi.java

--- demo1/src/Hi.java
+++ demo1/src/Hi.java
@@ -9,5 +9,9 @@ public class Hi {
     public static void main(String[] args) {
         System.out.println("hi~~");
         System.out.println("分支创建");
+        int i= 0;
+        if(i > -1){
+            System.out.printfln("你是对的");
+        }
     }
 }
